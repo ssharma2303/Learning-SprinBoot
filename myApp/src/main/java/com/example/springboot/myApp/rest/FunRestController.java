7dd567abfc379333c004ef8af59798b830f5504e
Sakshi Sharma <221910309044@gitam.in>
2023-09-20 23:01:18 +0530

    Files added

---
+++ myApp/src/main/java/com/example/springboot/myApp/rest/FunRestController.java
@@ -0,0 +1,23 @@
+package com.example.springboot.myApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+  // expose "/" that return "Hello World"
+
+  @GetMapping("/")
+  public String sayHello() {
+        return "Hello World!";
+    }
+ @GetMapping("/workout")
+  public String getDailyWorkout() {
+	  return "Run a 5k!";
+  }
+ @GetMapping("/fortune")
+  public String getFortune() {
+	  return "Today is your lucky day";
+  }
+}
---
+++ myApp/src/test/java/com/example/springboot/myApp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

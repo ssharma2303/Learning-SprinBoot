04f150a696a76541712345af1abd84b91701301d
Sakshi Sharma <221910309044@gitam.in>
2023-09-27 22:24:24 +0530

    "27-09-23"

--- 02-spring-boot-restcrud-employee-with-spring-datajpa/src/main/java/com/demo/cruddemo2/dao/EmployeeRepository.java
+++ 02-spring-boot-restcrud-employee-with-spring-datajpa/src/main/java/com/demo/cruddemo2/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.demo.cruddemo2.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.demo.cruddemo2.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
